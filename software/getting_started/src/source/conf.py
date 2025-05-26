14a5ab460e10ba12961553b0e21a9fa631d9ae92
Cesarbautista10 <cesar.bautista@uelectronis.com>
2025-05-26 16:33:24 -0600

    chore: build documentation for CH552 Multi-Protocol Programmer

--- software/getting_started/src/source/conf.py
+++ software/getting_started/src/source/conf.py
@@ -65,7 +65,7 @@ def setup(app):
 
 project = 'CH552 USB Multi-Protocol Programmer  User Guide and Technical Reference'
 copyright = '2025, Unit Electronics'
-author = 'Cesar Bautista'
+author = 'Department of Engineering, Innovation and Development'
 release = '0.0.1'
 extensions = [
     'sphinx.ext.autodoc',
@@ -100,7 +100,7 @@ html_theme_options = {
 }
 latex_documents = [
     ('index', 'programmer.tex', 'CH552 USB Multi-Protocol Programmer  User Guide and Technical Reference',
-     'Cesar Bautista', 'manual'),
+     'Department of Engineering, Innovation and Development', 'manual'),
 ]
 
 latex_elements = {
