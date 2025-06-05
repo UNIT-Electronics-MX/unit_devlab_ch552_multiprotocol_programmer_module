692abb1c56c41dfb352a4493a4a1a7d9de1c926f
Cesarbautista10 <cesar.bautista@uelectronis.com>
2025-06-05 17:36:04 -0600

    chore: upadte cmsisdap change ram

--- software/cmsisdap_/src/config.h
+++ software/cmsisdap_/src/config.h
@@ -6,14 +6,14 @@
 
 // Pin definitions
 #define PIN_LED             P11       // pin connected to LED, active low
-#define PIN_SWD             P15      // pin connected to SWDIO/TMS via 100R resistor
-#define PIN_SWK             P16       // pin connected to SWCLK/TCK via 100R resistor
+#define PIN_SWD             P16      // pin connected to SWDIO/TMS via 100R resistor
+#define PIN_SWK             P15       // pin connected to SWCLK/TCK via 100R resistor
 #define PIN_RST             P17       // pin connected to nRESET
 #define PIN_TDO             P14       // pin connected to TDO via 100R resistor
-#define PIN_TDI             P32       // pin connected to TDI via 100R resistor
-#define PIN_TRST            P34       // pin connected to nTRST
-#define PIN_RXD             P30       // pin connected to RXD via 470R resistor
-#define PIN_TXD             P31       // pin connected to TXT via 470R resistor
+#define PIN_TDI             P30       // pin connected to TDI via 100R resistor
+#define PIN_TRST            P31       // pin connected to nTRST
+#define PIN_RXD             P32       // pin connected to RXD via 470R resistor
+#define PIN_TXD             P34       // pin connected to TXT via 470R resistor
 
 // USB device descriptor
 #define USB_VENDOR_ID       0x1A86    // VID
@@ -24,8 +24,8 @@
 #define USB_MAX_POWER_mA    500       // max power in mA 
 
 // USB descriptor strings
-#define MANUFACTURER_STR    'C','o','c','k','e','t',' ','N','o','v','a'
-#define PRODUCT_STR         'C','o','c','k','e','t',' ','N','o','v','a',' ', 'C','M','S','I','S','-','D','A','P'
+#define MANUFACTURER_STR    'U','n','i','t','E','l','e','c','t','r','o','n','i','c','s'
+#define PRODUCT_STR          'M','u','l','t','i','P','r','o','t','o','c','o','l',' ', 'C','M','S','I','S','-','D','A','P'
 #define SERIAL_STR          'C','H','5','5','2'
 #define INTERFACE_STR_1     'C','D','C',' ','V','C','P'
 #define INTERFACE_STR_2     'H','I','D',' ','D','a','t','a'
--- software/sphinx/src/source/2_1_0_avr_firmware.rst
+++ software/sphinx/src/source/2_1_0_avr_firmware.rst
@@ -1,2 +1,26 @@
-AVR Firmware
-============
+AVR Firmware Overview
+=====================
+
+The CH552 Multi-Protocol Programmer relies on the PICO-AVR firmware to operate correctly. This firmware works with the CH552 USB-to-ISP bridge and is a critical part of the programmer's functionality. The CH55x-based picoAVR acts as a combined ISP, TPI, and UPDI programmer for AVR microcontrollers. It is compatible with both USBasp and SerialUPDI interfaces and integrates seamlessly with the Arduino IDE (accessible via Tools → Programmer → USBasp or Tools → Programmer → SerialUPDI).
+
+.. warning::
+
+    The PICO-AVR firmware is available under the
+    `Creative Commons Attribution-ShareAlike 3.0 Unported License <http://creativecommons.org/licenses/by-sa/3.0/>`_.
+
+    Additional resources:
+
+    - Github: `wagiminator <https://github.com/wagiminator>`_
+    - EasyEDA: `wagiminator at EasyEDA <https://easyeda.com/wagiminator>`_
+    - License details: `Creative Commons Attribution-ShareAlike 3.0 Unported License <http://creativecommons.org/licenses/by-sa/3.0/>`_
+
+Resources
+---------
+
+1. `EasyEDA Design Files <https://oshwlab.com/wagiminator>`_
+2. `WCH: CH552 Datasheet <http://www.wch-ic.com/downloads/CH552DS1_PDF.html>`_
+3. `SDCC Compiler <https://sdcc.sourceforge.net/>`_
+4. `Blinkinlabs: CH55x SDK for SDCC <https://github.com/Blinkinlabs/ch554_sdcc>`_
+5. `Thomas Fischl: USBasp <https://www.fischl.de/usbasp/>`_
+6. `Ralph Doncaster: USBasp <https://github.com/nerdralph/usbasp>`_
+7. `Deqing Sun: CH55xduino <https://github.com/DeqingSun/ch55xduino>`_
